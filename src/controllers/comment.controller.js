565608fef41ef4840a6373973559091f0b015e11
Khush509 <kothadiyakhush2@gmail.com>
2024-09-05 17:45:54 +0530

    Added router and controller structure for remaining models

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { offset = 0, limit = 10 } = req.query;
+
+  const offsetValue = req.query.offset || 0;
+  const limitValue = req.query.limit || 10;
+
+  const comments = await Comment.findById(videoId).skip(offsetValue).limit(limitValue)
+  const totalComments = await Comment.countDocuments()
+
+  return res.status(200).json(new ApiResponse(200, {totalComments: totalComments, comments}, "Comments fetched successfully"))
+});
+
+const addComment = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+    const { content } = req.body;
+
+    const video = Comment.findById(videoId)
+
+    if(!video){
+      throw new ApiError(404, "Video not found")
+    }
+
+    
+
+
+});
+
+const updateComment = asyncHandler(async (req, res) => {});
+
+const deleteComment = asyncHandler(async (req, res) => {});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
